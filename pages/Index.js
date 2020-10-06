cab211618584d2886f17b58500fe53ccc6436ce0
kristjanjansen <kristjan.jansen@gmail.com>
2020-10-06 10:33:43 +0200

    cleanup

--- pages/Index.js
+++ pages/Index.js
@@ -202,11 +202,11 @@ const EventRow = {
       
       <datetime :event="event" />
       <p style="line-height: 0.5em" />
-      <a v-if="event.id" target="_blank" style="display: block" :href="'./' + event.id">
+      <router-link v-if="event.id" style="display: block" :to="'/' + event.id">
         <div :class="pillClass">
           See the live at <b>{{ event.id }}.elektron.live</b> →
         </div>
-      </a>
+      </router-link>
     <div v-if="isOpen" v-html="event ? event.description : ''" />
     <br v-if="event && event.youtube" />
     <youtube v-if="event && event.youtube" :src="event.youtube" />
@@ -235,55 +235,55 @@ export default {
     return { currentEvents, pastEvents };
   },
   template: `
-  <main>
-  <div style="grid-area: header">
-    <img src="index.svg" style="width: 300px" />
-    <br />
-    <br />
+    <div class="layout-index">
+    <div style="grid-area: header">
+      <img src="index.svg" style="width: 300px" />
+      <br />
+      <br />
+    </div>
+    <div style="grid-area: events; display: grid; gap: 48px">
+      <h2>Upcoming events</h2>
+      <event-row v-for="event in currentEvents" :event="event"></event-row>
+      <h2>Past events</h2>
+      <event-row v-for="event in pastEvents" :event="event"></event-row>
+    </div>
+    <div style="grid-area: about; opacity: 0.7">
+      <h2>About</h2>
+      <br />
+      <br />
+      <p>
+        elektron.live can be considered as an online stage or a virtual
+        performative space. One of its main driving forces is to bring
+        performers and audiences together (with the use of a two-way
+        streaming) while minimizing the restrictions to access since any
+        number of audience members is possible.
+      </p>
+      <p>
+        When inventing and building elektron.live we realized that the
+        platform could be something more than only a way of demonstrating
+        performers and audience to each-other. We have never had a medium
+        before where there is a live interaction on both sides of the screen
+        simultaneously! We also learned that performing arts online could
+        never be the same as we know it from real encounters. With the
+        invention of a new virtual space we also have to invent a new kind of
+        performing arts. And that is something that really boosts our
+        curiosity in what to look for in the future. A whole new world in
+        arts!
+      </p>
+      <p>
+        Much like the first movies at the turn of 20'th century were just
+        recordings of the theatre performances, until now the online shows on
+        the internet are copies of the shows made in real life. Despite having
+        the internet for many decades, we are still inventing this medium for
+        performing arts. It took Dziga Vertov and his "Man with the movie
+        camera" to make movies look like movies. We are now creating a tool
+        for artists to do the same for the online performances.
+      </p>
+      <p>
+        elektron.live is created by e⁻lektron team together with Stepan
+        Bolotnikov and <a href="https://babahh.com">Babahh Media</a>.
+      </p>
+    </div>
   </div>
-  <div style="grid-area: events; display: grid; gap: 48px">
-    <h2>Upcoming events</h2>
-    <event-row v-for="event in currentEvents" :event="event"></event-row>
-    <h2>Past events</h2>
-    <event-row v-for="event in pastEvents" :event="event"></event-row>
-  </div>
-  <div style="grid-area: about; opacity: 0.7">
-    <h2>About</h2>
-    <br />
-    <br />
-    <p>
-      elektron.live can be considered as an online stage or a virtual
-      performative space. One of its main driving forces is to bring
-      performers and audiences together (with the use of a two-way
-      streaming) while minimizing the restrictions to access since any
-      number of audience members is possible.
-    </p>
-    <p>
-      When inventing and building elektron.live we realized that the
-      platform could be something more than only a way of demonstrating
-      performers and audience to each-other. We have never had a medium
-      before where there is a live interaction on both sides of the screen
-      simultaneously! We also learned that performing arts online could
-      never be the same as we know it from real encounters. With the
-      invention of a new virtual space we also have to invent a new kind of
-      performing arts. And that is something that really boosts our
-      curiosity in what to look for in the future. A whole new world in
-      arts!
-    </p>
-    <p>
-      Much like the first movies at the turn of 20'th century were just
-      recordings of the theatre performances, until now the online shows on
-      the internet are copies of the shows made in real life. Despite having
-      the internet for many decades, we are still inventing this medium for
-      performing arts. It took Dziga Vertov and his "Man with the movie
-      camera" to make movies look like movies. We are now creating a tool
-      for artists to do the same for the online performances.
-    </p>
-    <p>
-      elektron.live is created by e⁻lektron team together with Stepan
-      Bolotnikov and <a href="https://babahh.com">Babahh Media</a>.
-    </p>
-  </div>
-</main>
-`,
+  `,
 };
