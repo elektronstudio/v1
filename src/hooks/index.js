f91fd99c85ae0f8be7415e0765e70191aabadf36
kristjanjansen <kristjan.jansen@gmail.com>
2020-10-06 20:18:25 +0200

    add useLocalstore

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,4 @@
-import { ref, onMounted } from "../deps/vue.js";
+import { ref, onMounted, computed } from "../deps/vue.js";
 import { Hls } from "../deps/hls.js";
 
 export const useHls = (url) => {
@@ -21,4 +21,31 @@ export const useHls = (url) => {
     }
   });
   return el;
+};
+
+export const useLocalstore = (initialValue = null, key = null) => {
+  const value = ref(initialValue);
+  if (window.localStorage !== undefined) {
+    if (initialValue !== null && key && !window.localStorage.getItem(key)) {
+      window.localStorage.setItem(key, JSON.stringify(initialValue));
+    }
+    const localValue = computed({
+      get: () => {
+        let storedValue = null;
+        if (key && window.localStorage.getItem(key)) {
+          storedValue = JSON.parse(window.localStorage.getItem(key));
+          return storedValue !== value.value ? storedValue : value.value;
+        }
+        return value.value;
+      },
+      set: (val) => {
+        value.value = val;
+        if (key) {
+          window.localStorage.setItem(key, JSON.stringify(val));
+        }
+      },
+    });
+    return localValue;
+  }
+  return value;
 };
