d95410c4ed6a977515532dc8f84ad09e7509e051
kristjanjansen <kristjan.jansen@gmail.com>
2020-10-08 22:24:24 +0200

    cleanup

--- src/pages/Videotest.js
+++ src/pages/Videotest.js
@@ -18,7 +18,7 @@ const VideoStream = {
     return { videoEl };
   },
   template: `
-    <video ref="videoEl" autoplay></video>
+    <video ref="videoEl" autoplay style="width: 80px; height: 60px;"></video>
   `,
 };
 
@@ -29,7 +29,7 @@ export default {
     const subscribers = ref([]);
 
     const OV = new OpenVidu();
-    //OV.enableProdMode();
+    OV.enableProdMode();
     const session = OV.initSession();
 
     const url = "https://elektron.studio";
@@ -39,7 +39,6 @@ export default {
     session.on("streamCreated", ({ stream }) => {
       const subscriber = session.subscribe(stream);
       subscribers.value.push(subscriber);
-      console.log(subscribers.value);
     });
 
     session.on("streamDestroyed", ({ stream }) => {
@@ -65,7 +64,7 @@ export default {
         session.connect(token, { clientData: randomId() }).then(() => {
           const newPublisher = OV.initPublisher(null, {
             publishVideo: true,
-            resolution: "320x240",
+            resolution: "80x60",
             frameRate: 15,
             insertMode: "APPEND",
             mirror: false,
@@ -78,12 +77,34 @@ export default {
     return { publisher, subscribers };
   },
   template: `
-  <div class="live-layout">
-    <div style="grid-area: main">
-      <video-stream :stream="publisher" />
-      <p />
-      <video-stream v-for="stream in subscribers" :stream="stream" />
+  <div class="layout-live">
+    <div style="grid-area: logo; display: flex; align-items: center;">
+      <h1>Video demo</h1>&nbsp;&nbsp;&nbsp;
+      <router-link to="/"><div class="pill-gray">← Back to schedule</div></router-link>
     </div>
+    <div style="
+       grid-area: spec;
+       display: flex;
+       flex-wrap: wrap;
+     ">
+       <video-stream :stream="publisher" />
+       <video-stream v-for="stream in subscribers" :stream="stream" />
+     </div>
   </div>
   `,
+  // template: `
+  // <div class="index-layout">
+  //   <div style="
+  //     grid-area: chat;
+  //   ">aa</a>
+  //   <div style="
+  //     grid-area: main;
+  //     display: flex;
+  //     flex-wrap: wrap;
+  //   ">aa
+  //     <!-- <video-stream :stream="publisher" />
+  //     <video-stream v-for="stream in subscribers" :stream="stream" /> -->
+  //   </div>
+  // </div>
+  // `,
 };
