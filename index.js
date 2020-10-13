f43b1e34b9d65918f7a0cde15382dfc7ef185fe0
kristjanjansen <kristjan.jansen@gmail.com>
2020-10-13 10:13:04 +0200

    openvidu test

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ import Live from "./src/pages/Live.js";
 import Index from "./src/pages/Index.js";
 import Chattest from "./src/pages/Chattest.js";
 import Videotest from "./src/pages/Videotest.js";
+import Openvidu from "./src/pages/Openvidu.js";
+
 const routes = [
+  { path: "/openvidu", component: Openvidu },
   { path: "/chattest", component: Chattest },
   { path: "/videotest", component: Videotest },
   { path: "/:id", component: Live },
