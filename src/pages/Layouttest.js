4c2c6ebffe55e9bf752e5280906c6efe6a23184e
kristjanjansen <kristjan.jansen@gmail.com>
2020-10-21 17:54:59 +0200

    cleanup

--- src/pages/Layouttest.js
+++ src/pages/Layouttest.js
@@ -1,9 +1,23 @@
+import { ref, computed } from "../deps/vue.js";
+
 import AspectRatio from "../components/AspectRatio.js";
 
 export default {
   components: { AspectRatio },
+  setup() {
+    const chatOpen = ref(true);
+    const style = computed(() => {
+      return {
+        gridTemplateColumns: chatOpen.value
+          ? "3fr 1.5fr 1fr"
+          : "3fr 1.5fr 0.1fr",
+      };
+    });
+    const onClick = () => (chatOpen.value = !chatOpen.value);
+    return { onClick, style, chatOpen };
+  },
   template: `
-  <div class="layout-test">
+  <div class="layout-test" :style="style">
     <div style="border: 2px solid red; grid-area: performer">
       <aspect-ratio>
         <div style="padding: 15px">
@@ -17,8 +31,8 @@ export default {
       </div>
     </div>
     <div style="grid-area: chat">
-      <div style="border: 2px solid orange; position: sticky; top: 32px; padding: 15px; height: calc(100vh - 32px - 32px);">
-        Chat
+      <div @click="onClick" style="textAlign: right; cursor: pointer; border: 2px solid orange; position: sticky; top: 32px; padding: 15px; height: calc(100vh - 32px - 32px);">
+        {{ chatOpen ? '→' : '←' }}
       </div>
     </div>
     <div style="border: 2px solid red; padding: 15px; grid-area: about">
