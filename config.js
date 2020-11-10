fc9d92ba7f2c89a7635df350d03df073895eeae7
kristjanjansen <kristjan.jansen@gmail.com>
2020-11-10 22:37:55 +0100

    fix chat url

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ export const experimentalInputUrl = (id) =>
 export const specOutputUrl =
   "https://elektron-live.babahhcdn.com/bb1150-le/spectators/index.m3u8";
 
-//export const chatUrl = "wss://ws-fggq5.ondigitalocean.app";
+export const chatUrl = "wss://ws-fggq5.ondigitalocean.app";
 
-export const chatUrl = "ws://localhost:8080";
+//export const chatUrl = "ws://localhost:8080";
 
 export const eventsUrl =
   "https://www.googleapis.com/calendar/v3/calendars/mkr5k66b069hve1f7aa77m4bsc@group.calendar.google.com/events?key=AIzaSyAkeDHwQgc22TWxi4-2r9_5yMWVnLQNMXc";
