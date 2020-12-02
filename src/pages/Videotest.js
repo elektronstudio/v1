fe5abadb34f9c42e9345ad3deec9fc7eac96da61
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-02 15:38:11 +0100

    more cleanup

--- src/pages/Videotest.js
+++ src/pages/Videotest.js
@@ -1,8 +1,6 @@
 export default {
   template: `
   <div class="layout-videotest">
-    <!-- <div><audience-websocket /><p><br/>Sending still video frames via Websocket and broadcasting it to other clients. Small initial lag, 1fps updates, untested number of. No audio support.</p></div>
-    <div><audience-fetch /><p><br/>Sending still video frames using HTTPS POST request and returning image frames for all users. Small initial lag, 1fps updates, untested number of of participants. No audio support.</p></div> -->
     <div />
     <audience-openvidu id="test" />
     </div />
